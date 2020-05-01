81c76d43c86e21fd66f63498389edcc49604eb98
ShwetaBarge <bargeshweta983@gmail.com>
2020-05-01 14:17:00 +0530

    Assignment 5 completed

--- Week_5/ex4/nnCostFunction.m
+++ Week_5/ex4/nnCostFunction.m
@@ -61,24 +61,64 @@ Theta2_grad = zeros(size(Theta2));
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
+X = [ones(m,1) X]; % 5000*401
+z2 = Theta1 * X'; % (25*401)*(401*5000)
+a2 = sigmoid(z2); % (25*5000)
+
+a2 = [ones(m,1) a2'];
+z3 = Theta2 * a2';
+h_theta = sigmoid(z3); % h_theta equals a3
+
+y_new = zeros(num_labels, m); % 10*5000
+for i=1:m,
+  y_new(y(i),i)=1;
+end
+
+J = (1/m) * sum ( sum ( (-y_new) .* log(h_theta) - (1-y_new) .* log(1-h_theta) ));
+
+t1 = Theta1(:,2:size(Theta1,2));
+t2 = Theta2(:,2:size(Theta2,2));
 
 
+Reg = lambda  * (sum( sum ( t1.^ 2 )) + sum( sum ( t2.^ 2 ))) / (2*m);
 
 
+J = J + Reg;
+
+for t=1:m
+
+    
+	a1 = X(t,:);
+    a1 = a1'; 
+	z2 = Theta1 * a1;
+	a2 = sigmoid(z2);
+    
+    a2 = [1 ; a2]; 
+	z3 = Theta2 * a2; 
+	a3 = sigmoid(z3); 
+    
+
+	delta_3 = a3 - y_new(:,t); % (10*1)
+	
+    z2=[1; z2]; 
+    delta_2 = (Theta2' * delta_3) .* sigmoidGradient(z2); 
 
 
+	delta_2 = delta_2(2:end); 
+
+	Theta2_grad = Theta2_grad + delta_3 * a2'; 
+	Theta1_grad = Theta1_grad + delta_2 * a1'; 
+    
+end;
 
 
+Theta2_grad = (1/m) * Theta2_grad;
+Theta1_grad = (1/m) * Theta1_grad;
 
 
-
-
-
-
-
-
-
-
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + ((lambda/m) * Theta1(:, 2:end));  
+ 
+Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + ((lambda/m) * Theta2(:, 2:end)); 
 
 % -------------------------------------------------------------
 
